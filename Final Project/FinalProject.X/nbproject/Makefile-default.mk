3eb9f9b6df1b4352776651a702e4d11af63596c9
William Watkins <william.watkins@colorado.edu>
2021-12-11 10:28:38 -0700

    opening everything

--- Final Project/FinalProject.X/nbproject/Makefile-default.mk
+++ Final Project/FinalProject.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED="C:/Users/Will/Documents/GitHub/ASEN-5067-Microavionics/Final Project/FinalProject.X/FinalProjectMain.c"
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=C:/Users/Will/Documents/GitHub/ASEN-5067-Microavionics/Final Project/FinalProject.X/FinalProjectMain.c
 
 
 
@@ -94,7 +94,23 @@ MP_PROCESSOR_OPTION=18F87K22
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1: C:/Users/Will/Documents/GitHub/ASEN-5067-Microavionics/Final\ Project/FinalProject.X/FinalProjectMain.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}/_ext/45412932" 
+	@${RM} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1 "C:/Users/Will/Documents/GitHub/ASEN-5067-Microavionics/Final Project/FinalProject.X/FinalProjectMain.c" 
+	@-${MV} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.d ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1: C:/Users/Will/Documents/GitHub/ASEN-5067-Microavionics/Final\ Project/FinalProject.X/FinalProjectMain.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}/_ext/45412932" 
+	@${RM} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1 "C:/Users/Will/Documents/GitHub/ASEN-5067-Microavionics/Final Project/FinalProject.X/FinalProjectMain.c" 
+	@-${MV} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.d ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/45412932/FinalProjectMain.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
